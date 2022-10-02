284f132e02e8412dc70a6179b476970744002385
pushparajnanda <nsraj1990@gmail.com>
2022-10-02 18:39:13 +0530

    new code

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	
+	public void saving() {
+		System.out.println("Saving percentage is 6%");
+		
+	}
+	public void fixed() {
+		System.out.println("fixed percentage is 8%");
+	}
+	public void deposit() {
+		System.out.println("deposite percentage is 4%");
+	}
+
+}
